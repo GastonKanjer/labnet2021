6c01b7d99fc4aa5a68f53e8391ed5aea2f58dec5
Gaston Kanjer <gaston.kanjer@gmail.com>
2021-09-07 11:29:03 -0300

    Subido código HelloWorld
    Subido código junto con los archivos de proyecto y solución de HelloWorld

---
+++ LabNet2021_HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabNet2021_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
